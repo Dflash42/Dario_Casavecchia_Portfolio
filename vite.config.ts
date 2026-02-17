45b10030b56c8956f8c59c9f0d6d57ab0e7debcc
Dario Casavecchia <old.houseofhighlights@gmail.com>
2026-02-17 10:34:20 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/my-digital-showcase/',
+  base: '/Dario_Casavecchia_Portfolio/',
   server: {
     host: "::",
     port: 8080,
